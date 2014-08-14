0080738bf3f0f5a70e35eaad0a41ccecd232ee5e
Marcos Vinicius <ka-ladze@hotmail.com>
2014-08-13 23:31:36 -0300

    criando os models

---
+++ TDC/WebApplication1/Models/Palestra.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Palestra
+    {
+        public string Titulo { get; set; }
+        public string Codigo { get; set; }
+        public string Nivel { get; set; }
+        public string Descricao { get; set; }
+        public Trilha Trilha { get; set; }
+        public Sala Sala { get; set; }
+        public DateTime Horario { get; set; }
+        public Palestrante Palestrante { get; set; }
+    }
+}
---
+++ TDC/WebApplication1/Models/Palestrante.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Palestrante
+    {
+        public string Nome { get; set; }
+        public string Twitter { get; set; }
+        public string Bio { get; set; }
+        public String FotoId { get; set; }
+    }
+}
